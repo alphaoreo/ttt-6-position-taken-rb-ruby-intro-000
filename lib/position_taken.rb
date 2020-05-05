6dd4131333d1481c46d4ad067ac0840bab97c8a7
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-05-05 02:51:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken(board,index)
   if board[index]== "" || board[index]== " " || board[index]== "  " || board[index]== "   "
     return false
-  elsif board[index] == "X" || board[index]== "O"
+  elsif board[index] == "X" || board[index]== "O" || board[index] == nil
     return false
   else
     return true
